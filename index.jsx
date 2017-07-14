49c391da0a02b3511de87dd6f53ad5c8dfc52521
Allan Suaco <allan.suaco@gmail.com>
2017-07-14 17:30:42 +0800

    uploading bunch of file

---
+++ index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery'
+
+
+//   react code here
+
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        $("p").append(" <b>Appended text</b>.");
+    });
+
+    $("#btn2").click(function(){
+        $("ol").append("<li>Appended item</li>");
+    });
+});
+
+// react code here
+  
